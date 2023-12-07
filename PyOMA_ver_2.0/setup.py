fe47a084f54b454c701838173a3b2eb0650e8f20
Luca Landolfi <luca.landolfi@frontiere.io>
2023-12-07 15:13:56 +0100

    Add build files to PyOMA 2.0. Move source code

---
+++ PyOMA_ver_2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+VERSION = "1.5"
+DESCRIPTION = "PyOMA allows the experimental estimation of the modal parameters (natural frequencies, mode shapes, damping ratios) of a structure from measurements of the vibration response in operational condition."
+
+# Setting up
+setup(
+    name="Py-OMA",
+    version=VERSION,
+    author="Dag Pasquale Pasca, Angelo Aloisio, Marco Martino Rosso, Stefanos Sotiropoulos",
+    author_email="<supportPyOMA@polito.it>",
+    license="GNU General Public License v3 (GPLv3)",
+    description=DESCRIPTION,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dagghe/PyOMA",
+    packages=find_packages(),
+    install_requires=['numpy==1.23.4','scipy==1.9.3','pandas==1.5.1','matplotlib==3.6.2','seaborn==0.12.1','mplcursors==0.5.2'],
+    keywords=['operational modal analysis', 'ambient vibration modal test', 'structural dynamics', 'frequency domain decomposition', 'stochastic subspace identification', 'structural health monitoring'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
